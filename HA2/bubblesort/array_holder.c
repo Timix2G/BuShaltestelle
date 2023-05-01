db02c58090cd29f95d90f27025551419ca340263
Timix2G <timixlp@gmail.com>
2023-05-01 10:58:58 +0200

    probleme mit der Prozesserzeugung, Array wird aber auf shm geladen

--- HA2/bubblesort/array_holder.c
+++ HA2/bubblesort/array_holder.c
@@ -9,10 +9,20 @@
 
 
 bool isSorted(int array[]){
-    pid_t* child_pid;
-    posix_spawn(child_pid, "./bubble", NULL, NULL, NULL, NULL);
-    wait(*child_pid);
 
+    //Kindprozess erzeugen (hoffentlich)
+    int pidBuffer;
+    int* ptr = &pidBuffer;
+    char const* args[] = {"./bubble", MEM_NAME, NULL};
+    posix_spawn(ptr, *args, NULL, NULL, (char*const*)args, __environ);
+    
+    if(pidBuffer <= 0){
+        printf("Kindprozess konnte nicht erstellt oder pid nicht in den Buffer geschrieben werden\n");
+    }else{
+        wait(pidBuffer);
+    }
+
+    //auf Sortierung pruefen
     for(int i = 0; i < N_ELEMS; i++){
         if(array[i] > array[i+1]){
             return false;
@@ -22,28 +32,40 @@ bool isSorted(int array[]){
 }
 
 void printArray(int array[]){
+    //einfache Array-Ausgabe
     for (int i = 0; i < N_ELEMS; i++){
         printf("%d ", array[i]);
     }
 
     printf("\n");
-    sleep(0.001);
+}
+
+void fillArray(int array[]){
+    //einfaches fuellen des Arrays
+    for(int i = 0; i < N_ELEMS; i++){
+        array[i] = NUMBERS[i];
+    }
 }
 
 
 int main(){
+    //shared-memory oeffnen und auf NUMBERS vergroessern
     int shm_fd = shm_open(MEM_NAME, O_RDWR | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
-    ftruncate(shm_fd, N_ELEMS);
+    ftruncate(shm_fd, sizeof(NUMBERS));
     
-    int* shared_array = mmap(NULL, N_ELEMS, PROT_READ | PROT_WRITE, MAP_SHARED, shm_fd, 0);
-    *shared_array = *NUMBERS;
+    //das Array auf das shared-memory laden
+    int* shared_array = mmap(NULL, sizeof(NUMBERS), PROT_READ | PROT_WRITE, MAP_SHARED, shm_fd, 0);
+    fillArray(shared_array);
 
+    //auf sortierung pruefen
     while(!isSorted(shared_array)){
         printArray(shared_array);
     }
     
+    //fertig
     printf("Process finished: \n");
     printArray(shared_array);
 
+    //Verbindungen schließen
     close(shm_fd); shm_unlink(MEM_NAME); return 0;
 }
--- HA2/bubblesort/bubble.c
+++ HA2/bubblesort/bubble.c
@@ -4,20 +4,27 @@
 #include <fcntl.h>
 #include <unistd.h>
 
-int main(){
-    int shm_fd = shm_open(MEM_NAME, O_RDWR, 0);
-    int* shared_array = mmap(NULL, N_ELEMS, PROT_READ | PROT_WRITE, MAP_SHARED, shm_fd, 0);
-    
+void sort(int array[]){
+    //einfaches bubblesort ohne beachtung der schon sortierten Elemente
     for (int i = 0; i < N_ELEMS; i++){
 
-        if(shared_array[i + 1] < shared_array[i]){
-            int temp = shared_array[i];
-            shared_array[i] = shared_array[i+1];
-            shared_array[i+1] = temp;
+        if(array[i] > array[i + 1]){
+            int temp = array[i];
+            array[i] = array[i+1];
+            array[i+1] = temp;
         }
     }
-    
+}
 
-    munmap(shared_array, N_ELEMS); close(shm_fd);
+int main(){
+
+    //auf shared-memory zugreifen und array aus memory laden
+    int shm_fd = shm_open(MEM_NAME, O_RDWR, 0);
+    int* shared_array = mmap(NULL, sizeof(NUMBERS), PROT_READ | PROT_WRITE, MAP_SHARED, shm_fd, 0);
+
+    sort(shared_array);  
+
+    //Verbindungen schließen
+    munmap(shared_array, sizeof(NUMBERS)); close(shm_fd);
     return 0;
 }
