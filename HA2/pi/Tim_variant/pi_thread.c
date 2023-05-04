9d038282fec24334e8cd0cb50c18db947f43d0f7
Timix2G <timixlp@gmail.com>
2023-05-04 13:52:15 +0200

    funktioniert kind of aber irgendwie auch nicht (Alex hilfe)

--- HA2/pi/Tim_variant/pi_thread.c
+++ HA2/pi/Tim_variant/pi_thread.c
@@ -1,62 +1,69 @@
+#define _XOPEN_SOURCE
+#define _POSIX_C_SOURCE 199309L
 #include <stdio.h>
 #include <pthread.h>
 #include <stdlib.h>
 #include <time.h>
+#include <math.h>
 
-long long stepCounter = 0;
+long long stepCounter;
+long long points_in_circle;
+int t_threads;
 
-//idfk which/any return type 
-void generatePoint(long long *k, long long n){
-
-    while(n > 0){
+void* generatePoint(void* args){
+    double* iters_per_thread = (double*) args;
+    while(iters_per_thread[0] > 0){
         double x = drand48();
         double y = drand48();
 
         if(((x * x) + (y * y)) <= 1){
-            k++;
+            points_in_circle++;
         }
 
-        n--;
+        iters_per_thread[0]--;
         stepCounter++;
     }
     pthread_exit(NULL);
+    t_threads--;
 }
 
 int main(){
-    long long n;
-    long long t;
-    long long k;
+    long long n_iters;
+    stepCounter = 0;
+    points_in_circle = 0;
+    t_threads = 0;
     
-    //user input for iterations (n) nad threads (t)
+    //user input for iterations (n) and threads (t)
     printf("How many iterations?: \n");
-    scanf("%lld", &n);
-    printf("\nHow many threads?: \n");
-    scanf("%lld", &t);
+    scanf("%lld", &n_iters);
 
-    if(n > 0 && t > 0){
-        printf("n: %d t: %d\nstarting computation", n, t);
+    printf("\nHow many threads?: \n");
+    scanf("%d", &t_threads);
+
+    if(n_iters > 0 && t_threads > 0){
+        printf("n: %lld t: %d\nstarting computation", n_iters, t_threads);
     }else{
         perror("Iterations and/or threads can not be < 1");
         return 1;
     }
 
-    pthread_t threadIDs[t];
-    long long *args = {k, n/t};
+    pthread_t threadIDs[t_threads];
+    double iters_per_thread = ceil(((long double)n_iters)/ ((long double)t_threads));
+    double args = {iters_per_thread};
 
     //create threads
-    for(int i = 0; i < t; i++){
-        if(pthread_create(threadIDs[i], NULL, &generatePoint, *args) != 0){
+    for(int i = 0; i < t_threads; i++){
+        if(pthread_create(&threadIDs[i], NULL, &generatePoint, &args) != 0){
             perror("Thread creation failed");
             return 1;
         }
     }
 
     long double pi = 0; 
-    while(/*idfk*/){
-        pi = 4 * (((long double) k) / (long double) n);
-        print("%lld Steps completed.\nCurrently, Pi is: %lf\n", stepCounter, pi);
+    while(n_iters > stepCounter){
+        pi = 4 * (((long double) points_in_circle) / (long double) stepCounter);
+        printf("\n%lld Steps completed.\nCurrently, Pi is: %Lf\n, n_iters: %lld", stepCounter, pi, n_iters);
         nanosleep((const struct timespec[]){{1, 0}}, NULL);
     }
-
     return 0;
 }
